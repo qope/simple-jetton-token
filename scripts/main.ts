265342c00044755e2189aaa450c3e2a8580e3b8f
nuno <41233686+qope@users.noreply.github.com>
2024-06-12 16:18:05 +0200

    add contract

---
+++ scripts/main.ts
@@ -0,0 +1,40 @@
+import { mnemonicToPrivateKey } from '@ton/crypto'
+import {
+	Address,
+	JettonMaster,
+	JettonWallet,
+	TonClient,
+	WalletContractV3R2,
+	beginCell,
+	toNano,
+} from '@ton/ton'
+import dotenv from 'dotenv'
+dotenv.config()
+
+async function main() {
+	const tonCenterRPCURL = process.env.TON_CENTER_RPC_URL || ''
+	const client = new TonClient({
+		endpoint: tonCenterRPCURL,
+	})
+	const mnemonics = process.env.TON_MNEMONIC || ''
+	const keyPair = await mnemonicToPrivateKey(mnemonics.split(' '))
+	const wallet = WalletContractV3R2.create({
+		workchain: 0,
+		publicKey: keyPair.publicKey,
+	})
+	const walletContract = client.open(wallet)
+	const sender = walletContract.sender(keyPair.secretKey)
+
+	const address = walletContract.address
+	console.log('address', address)
+
+	const balance = await client.getBalance(address)
+	console.log('balance', balance)
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch(error => {
+		console.error(error)
+		process.exit(1)
+	})
---
+++ wrappers/SampleJetton.compile.ts
@@ -0,0 +1,6 @@
+import type { CompilerConfig } from '@ton/blueprint'
+
+export const compile: CompilerConfig = {
+	lang: 'tact',
+	target: 'contracts/sample_jetton.tact',
+}
