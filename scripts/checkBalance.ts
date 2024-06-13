1fde09e2bb5619487deb3ee6758c5a52d47a0146
nuno <41233686+qope@users.noreply.github.com>
2024-06-13 11:26:46 +0200

    update check balance

--- scripts/checkBalance.ts
+++ scripts/checkBalance.ts
@@ -1,13 +1,27 @@
 import dotenv from "dotenv";
 import { prepareClientWalletSender } from "./utils/prepare";
+import { SampleJetton } from "../wrappers/SampleJetton";
+import { Address } from "@ton/core";
+import { JettonDefaultWallet } from "../build/SampleJetton/tact_JettonDefaultWallet";
 dotenv.config();
 
 async function main() {
   const { client, wallet, sender } = await prepareClientWalletSender();
   const balance = await client.getBalance(wallet.address);
-
-  console.log("wallet address", wallet.address);
+  const owner = wallet.address;
+  console.log("ton wallet address", owner);
   console.log("ton balance", balance);
+
+  const jettonAddressString = process.env.TON_JETTON_ADDRESS || "";
+  const jettonAddress = Address.parse(jettonAddressString);
+  const jetton = client.open(SampleJetton.fromAddress(jettonAddress));
+  const jettonWalletAddress = await jetton.getGetWalletAddress(owner);
+  const jettonWallet = client.open(
+    JettonDefaultWallet.fromAddress(jettonWalletAddress)
+  );
+  console.log("jetton wallet address", jettonWalletAddress);
+  const jettonBalance = (await jettonWallet.getGetWalletData()).balance;
+  console.log("jetton balance", jettonBalance.toString());
 }
 
 main()
